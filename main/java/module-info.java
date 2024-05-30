fe681a0fbf4f89e51cb34cf38d5d97dc427b134b
saleemsgit <139800256+saleemsgit@users.noreply.github.com>
2024-05-31 00:24:24 +0530

    Delete main directory

--- main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.foodiesfavequeuemanagement {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.foodiesfavequeuemanagement to javafx.fxml;
-    exports com.example.foodiesfavequeuemanagement;
-}
